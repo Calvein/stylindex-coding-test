88afb4f07b899371db3bbf582e46199237a1f48c
Calvein <calvein@gmail.com>
2019-05-25 23:17:23 +0200

    add basic content with animation

--- src/Pokemon/index.js
+++ src/Pokemon/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
+import { Link } from '@reach/router'
+import { useSpring, animated } from 'react-spring'
+import styled from '@emotion/styled'
 
 const GET_POKEMON = gql`
   fragment pokemon on Pokemon {
@@ -51,15 +54,52 @@ const GET_POKEMON = gql`
   }
 `
 
+const Backdrop = styled(animated(Link))`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
+`
+
+const Content = styled(animated.div)`
+  position: fixed;
+  top: 15vh;
+  right: 5vh;
+  bottom: 0;
+  left: 5vh;
+  border-radius: 5px 5px 0 0;
+  background: #fff;
+`
+
 const Pokemon = ({ name }) => {
+  const backdropStyle = useSpring({ opacity: 1, from: { opacity: 0 } })
+  const contentStyle = useSpring({
+    to: { transform: 'translateY(0)' },
+    from: { transform: 'translateY(100%)' },
+  })
+
   const { data, loading, refetching, error } = useQuery(GET_POKEMON, {
     variables: { name },
   })
 
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+    return () => (document.body.style.overflow = 'auto')
+  }, [])
+
   if (loading && !refetching) return <h2>Loading...</h2>
   if (error) return <h2>Error: {error}</h2>
 
-  return <pre>{JSON.stringify(data.pokemon, null, 2)}</pre>
+  return (
+    <>
+      <Backdrop to="/" style={backdropStyle} />
+      <Content style={contentStyle}>
+        <pre>{JSON.stringify(data.pokemon, null, 4)}</pre>
+      </Content>
+    </>
+  )
 }
 
 export default Pokemon
--- src/PokemonList/index.js
+++ src/PokemonList/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import { Link } from '@reach/router'
+import styled from '@emotion/styled'
 
 const GET_POKEMONS = gql`
   query getPokemons($first: Int!) {
@@ -14,6 +15,24 @@ const GET_POKEMONS = gql`
   }
 `
 
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Pokemon = styled(Link)`
+  display: flex;
+  align-items: center;
+  &:nth-of-type(even) {
+    background: #f5f5f5;
+  }
+`
+
+const Img = styled.img`
+  height: 32px;
+  margin-right: 8px;
+`
+
 const PokemonList = ({ children }) => {
   const { data, loading, refetching, error } = useQuery(GET_POKEMONS, {
     variables: { first: 151 },
@@ -23,14 +42,14 @@ const PokemonList = ({ children }) => {
   if (error) return <h2>Error: {error}</h2>
 
   return (
-    <div>
-      {data.pokemons.map((pokemon) => (
-        <Link key={pokemon.id} to={pokemon.name}>
-          {pokemon.name}
-        </Link>
-      ))}
+    <List>
       {children}
-    </div>
+      {data.pokemons.map((pokemon) => (
+        <Pokemon key={pokemon.id} to={pokemon.name}>
+          <Img src={pokemon.image} />#{pokemon.number} {pokemon.name}
+        </Pokemon>
+      ))}
+    </List>
   )
 }
 
