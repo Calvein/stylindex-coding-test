28aba9be1e097a69e229874db6c56581c3ca8c28
Calvein <calvein@gmail.com>
2019-05-27 18:21:34 +0200

    Updates

---
+++ precache-manifest.264b1b9a34aee81cd64f0d4575ac362f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7753e7bdada65251869d2d71c47849",
+    "url": "/stylindex-coding-test/index.html"
+  },
+  {
+    "revision": "c028dbfc00a74d3a8b8d",
+    "url": "/stylindex-coding-test/static/js/2.e0954f8e.chunk.js"
+  },
+  {
+    "revision": "6c8287d72ee9a9cf9aec",
+    "url": "/stylindex-coding-test/static/js/main.23793b79.chunk.js"
+  },
+  {
+    "revision": "3840ee5199755e1fe0af",
+    "url": "/stylindex-coding-test/static/js/runtime~main.29a13861.js"
+  }
+]);
